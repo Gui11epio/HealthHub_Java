7a99d8dbeee7acf4b42f4d09f6d30732e977ed08
Gui11epio <guilherme.laiber@gmail.com>
2025-11-22 15:46:42 -0300

    começando o cache

--- src/main/java/br/com/fiap/chatia/ChatIaApplication.java
+++ src/main/java/br/com/fiap/chatia/ChatIaApplication.java
@@ -2,8 +2,10 @@ package br.com.fiap.chatia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ChatIaApplication {
 
     public static void main(String[] args) {
